e35858f2fe7dc6de6979041bba71fe26afe845a2
Vicky Kalogeiton <vicky.kalogeiton@gmail.com>
2017-10-06 16:59:17 +0200

    update cached data file

--- cache/fetch_cached_data.sh
+++ cache/fetch_cached_data.sh
@@ -3,31 +3,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-FILE=act-detector-cache.tgz
-CHECKSUM=7
+DATASET_NAME=$1
 
-if [ -f $FILE ]; then
-  echo "File already exists. Checking md5..."
-  os=`uname -s`
-  if [ "$os" = "Linux" ]; then
-    checksum=`md5sum $FILE | awk '{ print $1 }'`
-  elif [ "$os" = "Darwin" ]; then
-    checksum=`cat $FILE | md5`
-  fi
-  if [ "$checksum" = "$CHECKSUM" ]; then
-    echo "Model checksum is correct. No need to download."
-    exit 0
-  else
-    echo "Model checksum is incorrect. Need to download again."
-  fi
+declare -a ALL_DATASET_NAMES=("UCFSports" "JHMDB" "UCF-101")
+
+if [[ $ALL_DATASET_NAMES =~ (^|[[:space:]])$DATASET_NAME($|[[:space:]]) ]] ; then
+  echo "Downloading $DATASET_NAME ground truth data"
+else
+  echo "Dataset name does not exist. Please select a valid dataset."
+  exit 0 
 fi
 
-echo "Downloading ACT-detector cached ground truth data for UCFSports, J-HMDB and UCF-101 ..."
-
-wget http://thoth.inrialpes.fr/src/ACTdetector/downloads/act-detector-cache.tgz
-
-echo "Unzipping..."
-
-tar zxvf $FILE
-
-echo "Done. Please run this command again to verify that checksum = $CHECKSUM."
+wget http://pascal.inrialpes.fr/data2/act-detector/downloads/cache/$DATASET_NAME-GT.pkl
