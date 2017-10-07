bfbb905dd17722f0aefe2739fc725c7ac208c2e5
Vicky Kalogeiton <5147668+vkalogeiton@users.noreply.github.com>
2017-10-07 19:01:38 +0200

    Update fetch_initial_models.sh

--- models/ACT-detector/scripts/fetch_initial_models.sh
+++ models/ACT-detector/scripts/fetch_initial_models.sh
@@ -4,23 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 FILE=act-detector-initial-caffemodels.tgz
-CHECKSUM=d3347d864067ab3a42e3dc48fc50ed43
-
-if [ -f $FILE ]; then
-  echo "File already exists. Checking md5..."
-  os=`uname -s`
-  if [ "$os" = "Linux" ]; then
-    checksum=`md5sum $FILE | awk '{ print $1 }'`
-  elif [ "$os" = "Darwin" ]; then
-    checksum=`cat $FILE | md5`
-  fi
-  if [ "$checksum" = "$CHECKSUM" ]; then
-    echo "Model checksum is correct. No need to download."
-    exit 0
-  else
-    echo "Model checksum is incorrect. Need to download again."
-  fi
-fi
 
 echo "Downloading VGG16 caffemodels used as initialization for the RGB and the FLOW5 streams ..."
 
@@ -28,6 +11,6 @@ wget http://pascal.inrialpes.fr/data2/act-detector/downloads/initial_models/act-
 
 echo "Unzipping..."
 
-tar zxvf $FILE
+tar zxvf $FILE -C models/ACT-detector/ && rm -f $FILE
 
-echo "Done. Please run this command again to verify that checksum = $CHECKSUM."
+echo "Done."
